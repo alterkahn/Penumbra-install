f820c9d2344856827897075b608b681e292b376b
alterkahn <67752860+alterkahn@users.noreply.github.com>
2024-01-18 13:18:41 +0200

    Update penumbra.sh

--- penumbra.sh
+++ penumbra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download Penumbra CLI
-wget https://github.com/penumbra-zone/penumbra/releases/download/v0.64.1/pcli-x86_64-unknown-linux-gnu.tar.xz
+wget https://github.com/penumbra-zone/penumbra/releases/download/v0.64.2/pcli-x86_64-unknown-linux-gnu.tar.xz
 
 # Extract and rename
 tar -xvf pcli-x86_64-unknown-linux-gnu.tar.xz && mv pcli-x86_64-unknown-linux-gnu penumbra
@@ -9,11 +9,14 @@ tar -xvf pcli-x86_64-unknown-linux-gnu.tar.xz && mv pcli-x86_64-unknown-linux-gn
 # Clean up
 rm pcli-x86_64-unknown-linux-gnu.tar.xz
 
+# Changing directory
+cd penumbra
+
 # Initialize and generate keys
 ./pcli init soft-kms generate
 
 # View the address
 ./pcli view address
 
-# Changing directory and checking Penumbra balance
-cd penumbra && ./pcli view balance
+# Checking Penumbra balance
+./pcli view balance
