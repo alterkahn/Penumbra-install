ffcae59aa522097da5c6cce7462ea2955af140c5
alterkahn <67752860+alterkahn@users.noreply.github.com>
2024-03-01 11:48:01 +0200

    Update penumbra.sh

--- penumbra.sh
+++ penumbra.sh
@@ -3,8 +3,8 @@
 # Download Penumbra CLI
 wget https://github.com/penumbra-zone/penumbra/releases/download/v0.68.0/pcli-x86_64-unknown-linux-gnu.tar.xz
 
-# Extract and rename
-tar -xvf pcli-x86_64-unknown-linux-gnu.tar.xz && mv pcli-x86_64-unknown-linux-gnu/pcli /usr/bin
+# Extract and mv pcli binnary
+tar -xvf pcli-x86_64-unknown-linux-gnu.tar.xz && mv pcli-x86_64-unknown-linux-gnu/pcli /usr/bin && cd $HOME
 
 # Clean up
 rm -r pcli-x86_64-unknown-linux-gnu && rm pcli-x86_64-unknown-linux-gnu.tar.xz
